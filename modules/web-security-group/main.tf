08498eafb3235884c68470b915692c5c72233a7b
Ankit Agrawal <aankittcoolest@gmail.com>
2024-01-21 13:38:06 +0800

    Improving web security group rules

--- modules/web-security-group/main.tf
+++ modules/web-security-group/main.tf
@@ -8,6 +8,12 @@ resource "aws_security_group" "web_security_group" {
     protocol    = "tcp"
     cidr_blocks = var.cidr_blocks
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
 
   ingress {
     from_port   = 22
@@ -17,9 +23,9 @@ resource "aws_security_group" "web_security_group" {
   }
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "http"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
